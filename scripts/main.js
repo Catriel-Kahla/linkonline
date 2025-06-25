52071295592c0b99c05fa8dd4fc58f3a7b3e68f6
Catriel Gabriel Kahla <catrielgkahla@gmail.com>
2025-06-25 11:49:14 -0300

    commit

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,6 @@ let userCheck = {
     "pass": "8328"
 }
 
-if(!window.location.href.includes("www")){
-    window.location.href = 'https://www.linkonline.com.ar/'
-}
-
 function sessionIn () {
     if ((document.getElementById("idUser").value == userCheck.user)&&(document.getElementById("idPass").value == userCheck.pass)){
         document.getElementById("info").innerHTML = "Iniciando...";
