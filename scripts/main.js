852f68c3f7d9a876ffa38487a4869908eb6d61d1
Catriel Gabriel Kahla <94947330+Catriel-Kahla@users.noreply.github.com>
2025-06-25 10:10:58 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+let userCheck = {
+    "user": "admin",
+    "pass": "8328"
+}
+
+function sessionIn () {
+    if ((document.getElementById("idUser").value == userCheck.user)&&(document.getElementById("idPass").value == userCheck.pass)){
+        document.getElementById("info").innerHTML = "Iniciando...";
+        sessionStorage.setItem("user", document.getElementById("idUser").value)
+
+        setTimeout(function(){window.location.href = "/view/inicio.html"}, 2000);
+    } else {
+        document.getElementById("info").innerHTML = "X - Credenciales incorrectas";
+        setTimeout(function(){document.getElementById("info").innerHTML = ""}, 2000);
+    }
+}
